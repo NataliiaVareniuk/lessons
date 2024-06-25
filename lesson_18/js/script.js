460f49a6b67948dd3cfe624974db61a3dcd82b1b
Nataliia Vareniuk <162874110+NataliiaVareniuk@users.noreply.github.com>
2024-06-25 19:42:23 +0200

    Add files via upload

---
+++ lesson_18/js/script.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener('click', function (e) {
+	const targetElement = e.target;
+	if (targetElement.closest('.menu__button')) {
+		document.documentElement.classList.toggle('menu-open');
+	}
+})
